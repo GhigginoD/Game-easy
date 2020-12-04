1c2cfa5b6ee51427c90ec3224aea7c38aadbac56
dastar2 <davighiggino3@gmail.com>
2020-12-04 03:16:04 -0300

    Implementação da função 'create player'

--- core/main.py
+++ core/main.py
@@ -2,3 +2,10 @@ class Game():
     def create_estrutura(self):
         self.estrutura = [[0,0,0],[0,0,0],[0,0,0]]
 
+    def create_player(self):
+        self.resposta = str(input('deseja ser o player 1? [s/n]'))
+        if self.resposta == 's':
+            self.player = 1
+        else:
+            self.player = 2
+
